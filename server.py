182c2dff6d80b9efd26827ad6bb576f2affc337c
quaresmateo <teoian.giovane@gmail.com>
2020-12-18 18:10:10 -0300

    Adiciona debug do banco de dados

--- server.py
+++ server.py
@@ -6,6 +6,9 @@ import Pyro4
 db = orm.Database()
 
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+set_sql_debug(True)
+
+# db.generate_mapping(create_tables=True)
 
 
 @Pyro4.expose
